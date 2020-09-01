93548204ebdd01f77b0634b656fb42c94ccf8684
unknown <michaelangelomotta@gmail.com>
2020-09-01 01:18:58 -0400

    Resume section and more UI cleanup

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -67,6 +67,72 @@ export const About = () => {
             <div style={{ width: '80%' }}></div>
           </div>
         </div>
+
+        <div className="about__resume">
+          <h1 className="section-heading">Resume</h1>
+          <div className="bottom-line"></div>
+          <div className="about__resumeBlock">
+            <h2 className="section-heading">
+              MemoryBlue/User1st, Tysons Virginia
+            </h2>
+
+            <p className="sub-heading">Business Development Representative</p>
+            <p>(March 2019 - Present)</p>
+            <ul>
+              <li>Prospect for high quality leads using Salesforce CRM</li>
+              <li>Create high revenue generating opportunities</li>
+              <li>Fill sales pipeline</li>
+              <li>Training and mentorship</li>
+            </ul>
+          </div>
+          <div className="about__resumeBlock">
+            <h2 className="section-heading">
+              Motta&Motta Real Estate, Glen Burnie, MD
+            </h2>
+
+            <p className="sub-heading">Property Manager/Showing Agent</p>
+            <p>(2016 - Present)</p>
+            <ul>
+              <li>
+                Building and retaining a strong client base with Buildium CRM
+              </li>
+              <li>
+                Collaberating with contractors and landlords to keep properties
+                up to standards
+              </li>
+              <li>
+                Meet prospective renters at properties and give walkthroughs and
+                showcase details on the homes
+              </li>
+              <li>Organize and file corresponding paperwork</li>
+            </ul>
+          </div>
+          <div className="about__resumeBlock">
+            <h2 className="section-heading">
+              Hudson Coastal Raw Bar and Grill, Fulton MD
+            </h2>
+
+            <p className="sub-heading">Server/Bartender</p>
+            <p>(2016 - 2018)</p>
+            <ul>
+              <li>Serve guests food and beverages</li>
+              <li>Expedite food</li>
+              <li>Training and mentorship</li>
+            </ul>
+          </div>
+          <div className="about__resumeBlock">
+            <h2 className="section-heading">Education</h2>
+            <p>Howard Community College AA Business & Technology</p>
+            <p>
+              Reservoir High School(Fulton MD), High School Diploma Received
+            </p>
+            <h2 className="section-heading">Volunteer Experience</h2>
+            <p>
+              Maryland Food Bank, Packaging food and supplies for people in need
+              (2014 - Present)
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
