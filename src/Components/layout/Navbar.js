1f763281cab86e950f6639a43a1f826904d66f05
Mikey <michaelangelomotta@gmail.com>
2022-06-02 15:36:53 -0400

    More tweaks to nav bar icons

--- src/Components/layout/Navbar.js
+++ src/Components/layout/Navbar.js
@@ -7,25 +7,23 @@ export const Navbar = () => {
   return (
     <Fragment>
       <img src={profile} alt="Icon" className="responsive-img navbar__icon" />
-      <p className="flow-text navbar__title" style={{ margin: '1.3rem auto' }}>
-        Michaelangelo
-      </p>
+      
       <div className="navbar__iconBlock">
         <a
           href="https://www.linkedin.com/in/michaelangelo-motta/"
           className="navbar__link"
         >
-          <i class="fab fa-linkedin fa-2x"></i>
+          <i class="fab fa-linkedin fa-3x"></i>
           <span className="flow-text">LinkedIn</span>
         </a>
 
         <a href="tel:3015201800" className="navbar__link">
-          <i class="fas fa-phone-square fa-2x"></i>
+          <i class="fas fa-phone-square fa-3x"></i>
           <span className="flow-text">Phone</span>
         </a>
 
         <a href="mailto: michaelangelomotta@gmail.com" className="navbar__link">
-          <i class="fas fa-envelope fa-2x"></i>
+          <i class="fas fa-envelope fa-3x"></i>
           <span className="flow-text">Email</span>
         </a>
       </div>
